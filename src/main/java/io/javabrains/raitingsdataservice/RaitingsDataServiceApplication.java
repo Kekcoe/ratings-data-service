29f721197a69cdfab8ba2661cbb28ac044ea5861
kekcoe <kekcoe@gmail.com>
2023-02-24 15:22:30 +0300

    Created a new service and set port

---
+++ src/main/java/io/javabrains/raitingsdataservice/RaitingsDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.javabrains.raitingsdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaitingsDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaitingsDataServiceApplication.class, args);
+    }
+
+}
